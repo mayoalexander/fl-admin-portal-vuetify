a62d201dd94549d2ef242a8df2f7761523c30dbb
Alex Mayo <alex@freelabel.net>
2019-10-01 18:16:11 -0500

    configured the search app

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,15 +1,86 @@
 <template lang="html">
-  <div class="white--text">
-    this is the search
-    searchResults: {{ searchResults }}
-  </div>
+  <v-container>
+    <v-card
+      dark
+      background-color="darkBlue"
+      class="darkBlue elevation-1 py-2">
+        <div v-if="searchResults">
+          <v-data-table
+          :headers="headers"
+          :items="searchResults"
+          color="darkBlue"
+          hide-actions
+          dark
+          background-color="darkBlue"
+          class="darkBlue"
+        >
+          <template slot="items" slot-scope="props">
+            <td class="text-xs-right">
+              <img :src="props.item.photo" style="width:48px;height:48px;border-radius:50%;"/>
+            </td>
+            <!-- <td class="text-xs-right"></td> -->
+            <td class="text-xs-left">
+              <div class="ml-3 mb-3">
+                <h4 class="font-weight-bold mb-0">{{ props.item.name }}</h4>
+                Instagram: {{ props.item.instagram }}
+                <br>
+                Twitter: {{ props.item.twitter }}
+              </div>
+            </td>
+            <td class="text-xs-right">{{ props.item.city }} {{ props.item.state }}</td>
+            <td class="text-xs-right">
+              <v-btn dark small >
+                View
+              </v-btn>
+            </td>
+          </template>
+        </v-data-table>
+        </div>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      headers: [
+        {
+          sortable: false,
+          text: 'Profile',
+          value: 'name'
+        },
+        {
+          sortable: true,
+          text: 'City',
+          value: 'city',
+          align: 'right'
+        },
+        {
+          sortable: true,
+          text: 'Location',
+          value: 'city',
+          align: 'right'
+        },
+        {
+          sortable: false,
+          text: 'Actions',
+          value: 'actions',
+          align: 'right'
+        }
+      ]
+    }
+  },
   computed: {
     searchResults () {
-      return this.$store.state.searchResults
+      let result
+      const obj = this.$store.state.searchResults
+      if (obj) {
+        result = Object.keys(obj).map(function(key) {
+          return obj[key]
+        })
+      }
+      return result
     }
   }
 }
