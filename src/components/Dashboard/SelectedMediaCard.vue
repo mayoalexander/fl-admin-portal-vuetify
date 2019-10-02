ec333fce6b0dbce245e59d4b938864a1c15fd14b
Alex Mayo <alex@freelabel.net>
2019-10-02 08:51:21 -0500

    added the icons to the selected media card

--- src/components/Dashboard/SelectedMediaCard.vue
+++ src/components/Dashboard/SelectedMediaCard.vue
@@ -1,14 +1,21 @@
 <template lang="html">
-  <v-card class="elevation-1 pa-2 mb-3">
+  <v-card class="elevation-1 pa-2 mb-0">
     <v-layout row wrap>
       <v-flex xs3>
         <v-layout align-center justify-center>
-          <img style="height:80px;width:80px;" :src="selectedItem.photo" />
+          <img style="height:80px;width:80px;" :src="getPhoto(selectedItem)" />
         </v-layout>
       </v-flex>
       <v-flex xs9 class="pl-3">
         <h3 class="mt-0 mb-0">{{ selectedItem.title }}</h3>
-        <p class="mb-0">{{ selectedItem.twitter }}</p>
+        <p class="mb-0">
+          <i class="fa fa-twitter mr-1"></i>
+          <a :href="'https://twitter.com/' + selectedItem.twitter" target="_blank">{{ selectedItem.twitter }}</a>
+        </p>
+        <p class="mb-0">
+          <i class="fa fa-instagram mr-1"></i>
+          <a :href="'https://instagram.com/' + selectedItem.instagram.replace('@', '')" target="_blank">{{ selectedItem.instagram }}</a>
+        </p>
       </v-flex>
     </v-layout>
   </v-card>
@@ -18,6 +25,14 @@
 export default {
   props: {
     selectedItem: Object
+  },
+  methods: {
+    getPhoto (media) {
+      if (media.type === 'video') {
+        return 'https://i.ytimg.com/vi/' + media.media_id + '/hqdefault.jpg'
+      }
+      return this.selectedItem.photo
+    }
   }
 }
 </script>
