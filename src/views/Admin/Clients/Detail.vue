5304e8cc55594c500fdee7d867f2f009ed8e4aeb
Alex Mayo <alex@freelabel.net>
2020-02-12 20:48:04 -0600

    created blocks

--- src/views/Admin/Clients/Detail.vue
+++ src/views/Admin/Clients/Detail.vue
@@ -1,9 +1,52 @@
 <template>
-  <div class="px-4">
-    This is the customer detail page
-    <div class="white--text">
+  <div class="px-4 white--text">
+    <v-layout>
+      <v-flex xs12 class="px-2 mb-3 text-xs-center">
+        <div class="data-box pa-4">
+          Email & Phone + Dashboard Type
+        </div>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Date Created & Last Login
+        </div>
+      </v-flex>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Services
+        </div>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Profiles
+        </div>
+      </v-flex>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Activity
+        </div>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Billing
+        </div>
+      </v-flex>
+      <v-flex xs6 class="mb-3 text-xs-center px-2">
+        <div class="data-box pa-4">
+          Notifications
+        </div>
+      </v-flex>
+    </v-layout>
+
+    <!-- <div class="white--text">
       user: {{ user }}
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -28,5 +71,7 @@ export default {
 </script>
 
 <style>
-
+  .data-box {
+    background-color: #1c1f2a;
+  }
 </style>
